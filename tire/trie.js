2f0fe0d9704e9c9f96431606e0f87b49ad34a6cb
manumanuameeen <jerrykerry3806@gmail.com>
2025-08-21 12:28:59 +0530

    after delte

--- tire/trie.js
+++ tire/trie.js
@@ -1,39 +1,39 @@
 
 
 
-class TrieNode{
-    constructor(){
+class TrieNode {
+    constructor() {
         this.children = new Map();
         this.isEnd = false;
         this.meaning = null;
     }
 }
 
-class Trie{
-    
-    constructor(){
+class Trie {
+
+    constructor() {
         this.root = new TrieNode();
     }
 
-    insert(word,meaning){
+    insert(word, meaning) {
         let node = this.root;
-        for(let val of word){
-            if(!node.children.has(val)){
-                node.children.set(val,new TrieNode() )
+        for (let val of word) {
+            if (!node.children.has(val)) {
+                node.children.set(val, new TrieNode())
             }
-            node = node.children.get(val);           
+            node = node.children.get(val);
         }
 
 
 
         node.isEnd = true;
-     node.meaning= meaning?meaning:null;
+        node.meaning = meaning ? meaning : null;
     }
 
-    serch(word){
-        let node  = this.root;
-        for(let val of word){
-            if(!node.children.has(val)){
+    serch(word) {
+        let node = this.root;
+        for (let val of word) {
+            if (!node.children.has(val)) {
                 return false
             }
             node = node.children.get(val);
@@ -41,10 +41,10 @@ class Trie{
         return node.isEnd
     }
 
-    startWith(prefix){
+    startWith(prefix) {
         let node = this.root;
-        for(let val of prefix){
-            if(!node.children.has(val)){
+        for (let val of prefix) {
+            if (!node.children.has(val)) {
                 return false;
             }
             node = node.children.get(val)
@@ -52,59 +52,84 @@ class Trie{
         return true;
     }
 
-    autoSuggest(prefix){
+    autoSuggest(prefix) {
         let node = this.root;
-        for(let val of prefix){
-            if(!node.children.has(val)){
+        for (let val of prefix) {
+            if (!node.children.has(val)) {
                 return [];
             }
             node = node.children.get(val)
         }
         let result = [];
-        const dfs =(node, path)=>{
-           
-            if(node.isEnd === true){
-                result.push({word:path,meaning:node.meaning});
+        const dfs = (node, path) => {
+
+            if (node.isEnd === true) {
+                result.push({ word: path, meaning: node.meaning });
             }
-            for(let [val,children] of node.children){
-                   dfs(children,path+val)
+            for (let [val, children] of node.children) {
+                dfs(children, path + val)
             }
         }
-        dfs(node,prefix)
+        dfs(node, prefix)
         return result
     }
 
-    getMeaning(word){
+    getMeaning(word) {
         let node = this.root;
-        for(let val of word){
+        for (let val of word) {
             if (!node.children.has(val)) {
                 return null;
             }
-            node  = node.children.get(val);
+            node = node.children.get(val);
         }
-       return node.isEnd?node.meaning:null
+        return node.isEnd ? node.meaning : null
     }
 
+    
+         delleteWord  (word, node = this.root, depth = 0)  {
+            console.log("how many timer",depth)
+            console.log("currnode",node)
+            if (!node) return false;
+            if (depth === word.length) {
+                
+                if (!node.isEnd) return false;
+                node.isEnd = false
+                return node.children.size === 0
+            }
+            let char = word[depth];
+            if (!node.children.has(char)) return false
+        
+            const shouldbeDelete = this.delleteWord(word, node.children.get(char), depth + 1);
+            if (shouldbeDelete) {
+                
+                node.children.delete(char)
+                return node.children.size === 0 && !node.isEnd 
+            }
+            return false
+        }
+    
 
-    display(){
+    display() {
         console.log(this.root)
     }
 }
 const tr = new Trie();
-tr.insert("cat","its a animal can be taimed and we can care in our home");
-tr.insert("car","its a vehicle have four wheels");
-tr.insert("cart","its a blublublu");
-tr.insert("cow","its a animal  get milk from this animal ");
-tr.insert("water","this is a liquid we can drink ,it's very healthy for our health ");
+tr.insert("cat", "its a animal can be taimed and we can care in our home");
+tr.insert("car", "its a vehicle have four wheels");
+// tr.insert("cart", "its a blublublu");
+// tr.insert("cow", "its a animal  get milk from this animal ");
+// tr.insert("water", "this is a liquid we can drink ,it's very healthy for our health ");
+// console.log(tr.serch("car"))
+// console.log(tr.serch("cow"))
+// console.log(tr.startWith("ca"))
+// console.log(tr.autoSuggest("car"))
+// console.log(tr.getMeaning("cow"))
+// console.log(tr.getMeaning("car"))
+// console.log(tr.getMeaning("cat"))
+// console.log(tr.getMeaning("water"))
+tr.delleteWord("cat")
 console.log(tr.serch("cat"))
-console.log(tr.serch("car"))
-console.log(tr.serch("cow"))
-console.log(tr.startWith("ca"))
- console.log(tr.autoSuggest("car")) 
-console.log(tr.getMeaning("cow"))
-console.log(tr.getMeaning("car"))
-console.log(tr.getMeaning("cat"))
-console.log(tr.getMeaning("water"))
+
 // tr.display()
 
 
