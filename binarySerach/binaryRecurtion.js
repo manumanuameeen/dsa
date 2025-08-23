aee30c15e3a5c171f25bedd7b11ed58c8f2bed1f
manumanuameeen <jerrykerry3806@gmail.com>
2025-08-23 17:46:28 +0530

    after bs

--- binarySerach/binaryRecurtion.js
+++ binarySerach/binaryRecurtion.js
@@ -1,20 +1,25 @@
 function binarySearch(arr,high, low, target) {
-
     let mid = Math.floor((high + low) / 2)
+   
 
     if (low > high) return -1;
 
-    if (arr[mid] === target) {
-         arr[mid] = 0;
-         return true
+    if (arr[mid].id === target) {
+ 
+         return arr[mid].name
     }
-    if (arr[mid] > target) {
+    if (arr[mid].id > target) {
         return binarySearch(arr,mid - 1,low, target);
-    } else if (arr[mid] < target) {
+    } else if (arr[mid].id < target) {
         return binarySearch(arr,high, mid +1, target);
     }
 }
-let arr = [1,2,3,4,5,6];
+let arr = [
+  { id: 1, name: "Alice" },
+  { id: 3, name: "Bob" },
+  { id: 5, name: "Charlie" }
+];
 
-console.log(binarySearch(arr,arr.length-1,0,100))
-console.log(arr)
+
+console.log(binarySearch(arr,arr.length-1,0,3))
+// console.log(arr[])
