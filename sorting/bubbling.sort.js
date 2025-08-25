cae4f59caa3a45ccc1d2f5223d40a010ab5fcb6b
manumanuameeen <jerrykerry3806@gmail.com>
2025-08-25 10:07:06 +0530

    after bubbling

--- sorting/bubbling.sort.js
+++ sorting/bubbling.sort.js
@@ -1,20 +1,20 @@
 
+function bubbling(arr){
+    
+    for(let i =0 ;i<arr.length-1;i++){
+        let swapped = false;
 
-let arr = [1,3,5,6,32];
-let n= arr.length;
-
-for(let i =0 ; i < n ;i++){
-
-    let swapped = false;
-
-    for(let j =0 ; j < n - 1 - i ; j++){
-        if(arr[j] > arr[j+1]){
-            [arr[j], arr[j+1]] =      [arr[j+1], arr[j]]
-            swapped = true;
+        for(let j =0 ; j<arr.length-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                [arr[j],arr[j+1]] = [arr[j+1],arr[j]];
+                swapped = true;
+            }
         }
+        if(!swapped)break;
     }
-    if(!swapped){
-        break;
-    }
+    return arr;
+}
+let arr = [3,6,2,6,89] 
+console.log(bubbling(arr));
 
-}console.log(arr)
+
